28595ebfc5be020614425e5d04ad6ad3f2e851d4
Amirulhkmi <amirulmunadhil01@gmail.com>
2025-08-18 10:24:39 +0800

    Update db_connect.php
    change the db connection to connect with the railway

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,12 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "intern_attendance_db";
+// Use the credentials provided by Railway (MySQL plugin)
+$host = "mysql.railway.internal";  // <-- Railway host
+$port = 3306;                                 // <-- Railway port
+$user = "root";                               // <-- Railway user
+$pass = "UyIfPlnMFcROjQPDwqUhOigqXbrfhZjV";              // <-- Railway password
+$db   = "railway";                            // <-- Railway database name
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
